7cd9cbd591e57869ae86b89f161ac89b5fc111c3
Sumit Kumar Garsa <sumitkumargarsa@gmail.com>
2024-11-05 21:48:18 +0530

    main

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Nav from "@/components/Home/Nav";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "iloveecommerce",
+  description: "Webdev warriors shop using next js 14",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}<Nav/></body>
+    </html>
+  );
+}
